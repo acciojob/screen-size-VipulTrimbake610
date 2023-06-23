aabe36e43c342f04489fe727451baf99caf104d0
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-23 06:24:05 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const body = document.body;
+const w = window.getComputedStyle(body);
+console.log(w.width);
+
+body.addEventListener("resize",function(){
+	console.log("hello");
+})
+
+
