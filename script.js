b3a28e2fec7a72d5ea4db92bab26b9ab7def930e
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-23 06:44:09 -0700

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const h1 = document.getElementsByTagName("h1")[0];
 
 
 window.addEventListener("resize",function(){
+	div.style.width = window.innerWidth;
+	div.style.height = window.innerHeight;
+	
 	h1.innerHTML = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
 })
 
